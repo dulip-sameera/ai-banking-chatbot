80fa814f9bc9e73932bbeba5376d0f02b14afd57
Dulip Sameeera <dsgitrepo101@gmail.com>
2025-05-30 10:19:41 +0530

    create main.py

---
+++ app/main.py
@@ -0,0 +1,18 @@
+import nltk
+from dotenv import load_dotenv
+import tkinter as tk
+from chatbot_ui import ChatbotUI
+
+# Download NLTK data
+nltk.download('punkt')
+nltk.download('punkt_tab')
+nltk.download('wordnet')
+nltk.download('stopwords')
+
+load_dotenv()
+
+if __name__ == "__main__":
+    root = tk.Tk()
+    app = ChatbotUI(root)
+    root.mainloop()  
+            
